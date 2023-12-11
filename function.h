f92979f00b387ef1f9653c94863aac0fac4bf42c
DavidEtoedia <etoedia@gmail.com>
2023-12-11 13:45:13 -0800

    begins

---
+++ function.h
@@ -0,0 +1,22 @@
+#ifndef FUNCTION_H
+#define FUNCTION_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+
+#include "shelldata.h"
+
+extern char **environ;
+
+void begin(shelldata_t *mytype);
+int number(int num);
+char *stringify(int numbe);
+int isNumeric(unsigned int number);
+int hasLetter(char *string);
+
+#endif
